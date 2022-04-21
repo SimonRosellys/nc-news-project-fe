25396a6db8ad89ce70fd6cbe81839db8bcc283ed
Simon Rosellys <simonrosellys@aol.com>
2022-04-21 16:59:32 +0100

    Complete vote increment and decrement

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -2,26 +2,45 @@ import { useState } from "react";
 
 const Votes = ({ votes, article_id }) => {
   const [optimisticVotes, setOptimisticVotes] = useState(0);
+  const [voted, setVoted] = useState(false);
 
-  const incrementVotes = () => {
-    setOptimisticVotes((currentVotes) => currentVotes + 1);
+  const amendVotes = (num) => {
+    setOptimisticVotes((currentVotes) => currentVotes + num);
     fetch(
       `https://nc-news-project-sr.herokuapp.com/api/articles/${article_id}`,
       {
         method: "PATCH",
-        body: JSON.stringify({ inc_votes: 1 }),
+        body: JSON.stringify({ inc_votes: num }),
         headers: { "Content-Type": "application/json" },
       }
     ).catch(() => {
-      setOptimisticVotes((currentVotes) => currentVotes - 1);
+      setOptimisticVotes((currentVotes) => currentVotes + num);
+      return <p>Vote not counted, please try later</p>;
     });
   };
+
+  //
 
   return (
     <div>
       <p>Votes: {votes + optimisticVotes}</p>
-      <button onClick={incrementVotes} disabled={optimisticVotes > 0}>
+      <button
+        disabled={voted}
+        onClick={() => {
+          amendVotes(1);
+          setVoted(true);
+        }}
+      >
         Add a Vote
+      </button>
+      <button
+        disabled={!voted}
+        onClick={() => {
+          amendVotes(-1);
+          setVoted(false);
+        }}
+      >
+        Remove your Vote
       </button>
     </div>
   );
